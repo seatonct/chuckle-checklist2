3aae09f7c43191a6821c380ce4158e60dfdf0c89
Charles Seaton <seatonct@gmail.com>
2023-08-30 13:19:38 -0500

    finished chapter 2

--- src/services/jokeService.js
+++ src/services/jokeService.js
@@ -1,3 +1,20 @@
+export const postJoke = async (joke) => {
+  const jokeObj = {
+    text: joke,
+    told: false,
+  };
+
+  const postInput = {
+    method: "Post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jokeObj),
+  };
+
+  const response = await fetch("http://localhost:8088/jokes", postInput);
+};
+
 export const getAllJokes = () => {
-  return fetch(`http://localhost:8088/jokes`).then((res) => res.json());
+  return fetch("http://localhost:8088/jokes").then((res) => res.json());
 };
